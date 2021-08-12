95bf62e5651ba38f5b04454890aa9c559f1b5bf1
Ola <Ola@example.com>
2021-08-12 09:10:21 +0900

    test app, first commit

---
+++ app_test/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import AppViewIdx, AppViewPage01
+
+app_name = 'app_name_01'
+
+urlpatterns = [
+    path('ns_idx/',
+         AppViewIdx.as_view(template_name='app_test/index.html'),
+         name='name_idx'),
+    path('ns_pg01/',
+         AppViewPage01.as_view(template_name='app_test/page01.html'),
+         name='name_pg01'),
+]
---
+++ app_test/views.py
@@ -0,0 +1,17 @@
+from django.views.generic.base import TemplateView
+
+
+class AppViewIdx(TemplateView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["app"] = "Index"
+        return context
+
+
+class AppViewPage01(TemplateView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["app"] = "Page01"
+        return context
